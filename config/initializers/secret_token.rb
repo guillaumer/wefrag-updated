97a2989761b99454c1f525771707e0b953158dee
Pierre Bellon <bellon.pierre@gmail.com>
2011-09-28 12:21:12 +0200

    Init of repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WefragUpdated::Application.config.secret_token = '65470ea07f991827fc6a34230428fdb2235935c202512db20cd4b3484bfdef82a2f5a4ad03dc5bb99e62fa98f02804b58ba2e74ee4a79ce75782e87ad94a0674'
